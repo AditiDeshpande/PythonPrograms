8f44609eaba394d15f99d2c125841de3f1694e7c
aditi <aditid11@gmail.com>
2020-06-25 22:54:58 +0530

    List in Python

---
+++ sequences.py
@@ -0,0 +1,19 @@
+# List in Python
+
+name = "Aditi"
+coordinates = (10.0 , 20.0)
+friends = ["Aditi" , "Priyada" , "Deepti" , "Siddhi" ,
+"Mayur" , "Nikhil" , "Sumant" , "Vivek"]
+
+print(f"Fourth letter in {name} is " + name[3])
+
+print("x co-ordinate is")
+print(coordinates[0])
+
+print("6 th friend in the list is " + friends[5])
+
+# Output
+# Fourth letter in Aditi is t
+# x co-ordinate is
+# 10.0
+# 6 th friend in the list is Nikhil
