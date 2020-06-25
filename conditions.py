cb480f758916e9fc180c319cebb5bc2bb722413c
aditi <aditid11@gmail.com>
2020-06-25 20:13:54 +0530

    Conditions in Python.

---
+++ conditions.py
@@ -0,0 +1,16 @@
+name = "Person1"
+x = 0
+
+if x > 0:
+    print(f"As x is positive {name} is Corona Positive")
+
+elif x < 0:
+    print(f"As x is negative {name} is Corona Negative")
+
+else:
+    print(f"As x is 0 {name} is Fully Recovered and Healthy!!! :) ")
+
+#indentation is must in Python otherwise it gives error :P :) super cool
+
+#Output
+# As x is 0 Person1 is Fully Recovered and Healthy!!! :) 
