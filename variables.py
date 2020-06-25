d2d21edc672cbbdd06fdc814d17366ba7f1a4f87
aditi <aditid11@gmail.com>
2020-06-25 20:01:49 +0530

    4 types of variables in Python sample prog.

---
+++ variables.py
@@ -0,0 +1,20 @@
+i = 28
+print(f"i is {i}")
+
+f = 2.8
+print(f"f is {f}")
+
+b = True
+print(f"b is {b}")
+
+n = None
+print(f"none is {n}")
+
+
+#Output
+# i is 28
+# f is 2.8
+# b is True
+# none is None
+# None is variable which is not having ANY value. Means it doesn't store
+#any information
