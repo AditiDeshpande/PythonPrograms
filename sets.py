7c3f0976424589f5dcf2979ecae15796749a6f95
aditi <aditid11@gmail.com>
2020-06-26 00:13:34 +0530

    Sets

---
+++ sets.py
@@ -0,0 +1,17 @@
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(6)
+# if 2 same entries in the sets in output it's showing that
+#element only once
+#s.add(5)
+
+print(s)
+
+#Output
+# {1, 2, 3, 4, 5, 6}
+# {1, 2, 3, 4, 5}
