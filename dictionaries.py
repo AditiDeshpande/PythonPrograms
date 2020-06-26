b9b4b10e928c3531cf06abc3c11e702ebf07adb0
aditi <aditid11@gmail.com>
2020-06-26 11:01:56 +0530

    Sets and Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+ages = {"Vivek": 56 , "Milind" : 57}
+ages["Vaishnavi"] = 34
+ages["Vivek"] -= 1
+
+print(ages)
+
+#Output
+#{'Vivek': 55, 'Milind': 57, 'Vaishnavi': 34}
--- sets.py
+++ sets.py
@@ -6,6 +6,11 @@ s.add(3)
 s.add(4)
 s.add(5)
 s.add(6)
+s.add("aditi")
+s.add("Painting")
+s.add(6.5)
+s.add(6.55)
+s.add("24lacs")
 # if 2 same entries in the sets in output it's showing that
 #element only once
 #s.add(5)
