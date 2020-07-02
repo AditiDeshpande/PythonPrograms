c3a7c75d775a40c7d1c6c9b389069d1e5998e5ad
aditi <aditid11@gmail.com>
2020-07-03 01:08:53 +0530

    Using lambda functions.

--- lambda.py
+++ lambda.py
@@ -4,15 +4,18 @@ makharTypes = [
     {"name" : "Saraswati Design" , "Make" : "MDF + Paint"}
 ]
 
-def types(makharTypes):
-    return makharTypes["Make"]
-
-makharTypes.sort(key = types)
+makharTypes.sort(key = lambda makharTypes: makharTypes["name"])
+#lambda input / parameter to function : output
 
 print(makharTypes)
 
 
-#Output
-# [{'name': 'Sinhasan', 'Make': 'Cardboard'}, 
+#Output foll is sorted by make
+# [{'name': 'Sinhasan', 'Make': 'Cardboard'},
 # {'name': 'Saraswati Design', 'Make': 'MDF + Paint'},
 # {'name': 'Mandir Pattern', 'Make': 'MDF + Velvet'}]
+
+#Output foll is sorted by name using lambda function
+# [{'name': 'Mandir Pattern', 'Make': 'MDF + Velvet'},
+#  {'name': 'Saraswati Design', 'Make': 'MDF + Paint'},
+#  {'name': 'Sinhasan', 'Make': 'Cardboard'}]
