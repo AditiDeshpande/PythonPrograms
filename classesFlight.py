0e973532e0b70a24d90a1ef3c24d4ec58205d107
aditi <aditid11@gmail.com>
2020-07-03 00:31:49 +0530

    Another sample for Classes in Python.

---
+++ classesFlight.py
@@ -0,0 +1,31 @@
+class Flight():
+    def __init__(self , capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self , name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Harry" , "Harmione" ,"Ron" , "Ginny"]
+
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight succesfully !!")
+    else:
+        print(f"No available seats for {person}")
+
+
+#Output
+# Added Harry to flight succesfully !!
+# Added Harmione to flight succesfully !!
+# Added Ron to flight succesfully !!
+# No available seats for Ginny
