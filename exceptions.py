dc589e762a184e3be7832f91d05e312c3168d7ab
aditi <aditid11@gmail.com>
2020-07-03 10:57:02 +0530

    Exception handling in Python.

---
+++ exceptions.py
@@ -0,0 +1,20 @@
+import sys
+
+x = int(input("X : "))
+y = int(input("Y : "))
+
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: Can not divide by zero.")
+    sys.exit(1)
+
+print(f" {x} / {y} = {result}")
+
+# X = 10 Y = 0 Divide by zero error
+# How to handle exceptions
+#Following is the exception thrown
+# Traceback (most recent call last):
+#   File "exceptions.py", line 7, in <module>
+#     result = x / y
+# ZeroDivisionError: division by zero
